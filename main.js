0ab81484d51865d1a4f238f26a9621acb21252c1
Beerangi Rajani <rajani.coding@gmail.com>
2026-03-10 22:36:26 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,4 +2,6 @@
 document.querySelector('.nav__toggle').addEventListener('click', () => {
   const navList = document.querySelector('.nav__list');
   navList.style.display = navList.style.display === 'flex' ? 'none' : 'flex';
+  
 });
+
