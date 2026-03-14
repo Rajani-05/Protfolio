8deafd046db5836f95ff5224f3f09c42b0331480
Beerangi Rajani <rajani.coding@gmail.com>
2026-03-15 02:30:56 +0530

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-// main.js
-document.querySelector('.nav__toggle').addEventListener('click', () => {
-  const navList = document.querySelector('.nav__list');
-  navList.style.display = navList.style.display === 'flex' ? 'none' : 'flex';
-  });
-
-
